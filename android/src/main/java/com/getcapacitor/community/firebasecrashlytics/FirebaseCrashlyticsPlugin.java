2c411903c43da0ff108a0e25c86dd26d69caa277
Robin Genz <mail@robingenz.dev>
2021-04-17 23:01:49 +0200

    refactor: android

--- android/src/main/java/com/getcapacitor/community/firebasecrashlytics/FirebaseCrashlyticsPlugin.java
+++ android/src/main/java/com/getcapacitor/community/firebasecrashlytics/FirebaseCrashlyticsPlugin.java
@@ -21,185 +21,112 @@ import com.google.firebase.FirebaseApp;
 )
 public class FirebaseCrashlyticsPlugin extends Plugin {
 
-    public static final String TAG = "FirebaseCrashlytics";
-    private Bridge bridge;
+    public static final String ERROR_MESSAGE_MISSING = "message must be provided.";
+    public static final String ERROR_KEY_MISSING = "key must be provided.";
+    public static final String ERROR_VALUE_MISSING = "value must be provided.";
+    public static final String ERROR_USERID_MISSING = "userId must be provided.";
+    public static final String ERROR_ENABLED_MISSING = "enabled must be provided.";
+    private FirebaseCrashlytics implementation;
 
     @Override
     public void load() {
-        if (bridge == null) {
-            bridge = this.getBridge();
-        }
-
-        if (FirebaseApp.getApps(bridge.getContext()).size() == 0) {
-            FirebaseApp.initializeApp(bridge.getActivity().getApplicationContext());
-        }
-
-        super.load();
+        implementation = new FirebaseCrashlytics();
     }
 
-    /**
-     * Trigger a fatal crash for crashlytics to record
-     * @param call
-     */
     @PluginMethod
     public void crash(PluginCall call) {
-        if (!call.hasOption("message")) {
-            call.reject("message property is missing");
+        String message = call.getString("message");
+        if (message == null) {
+            call.reject(ERROR_MESSAGE_MISSING);
             return;
         }
-
-        String message = call.getString("message");
         call.resolve();
-        throw new RuntimeException(message);
+        implementation.crash(message);
     }
 
-    /**
-     * Sets a custom key and value to be associated with subsequent fatal and non-fatal reports. When setting an object value, the object is converted to a string.
-     * @param call - key: a unique key
-     *               value: a value to be associated with the given key
-     */
     @PluginMethod
     public void setContext(PluginCall call) {
-        if (!call.hasOption("key") || !call.hasOption("value")) {
-            call.reject(!call.hasOption("key") ? "key property is missing" : "value propery is missing");
+        String key = call.getString("key");
+        if (key == null) {
+            call.reject(ERROR_KEY_MISSING);
             return;
         }
-
-        try {
-            String key = call.getString("key");
-            String type = call.getString("type", "string");
-
-            switch (type) {
-                case "string":
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, call.getString("value"));
-                    break;
-                case "long":
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, Long.valueOf(call.getInt("value")));
-                    break;
-                case "int":
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, call.getInt("value"));
-                    break;
-                case "boolean":
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, call.getBoolean("value"));
-                    break;
-                case "float":
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, call.getFloat("value"));
-                    break;
-                case "double":
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, call.getDouble("value"));
-                    break;
-                default:
-                    com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCustomKey(key, call.getString("value"));
-            }
-            call.resolve();
-        } catch (Exception ex) {
-            Log.e(TAG, "Caught exception while setting a crashlytics context: " + ex.getLocalizedMessage());
+        boolean hasValue = call.hasOption("value");
+        if (!hasValue) {
+            call.reject(ERROR_VALUE_MISSING);
+            return;
         }
+        String type = call.getString("type", "string");
+        implementation.setContext(key, type, call);
+        call.resolve();
     }
 
-    /**
-     * Records a user ID (identifier) that’s associated with subsequent fatal and non-fatal reports.
-     * @param call - userId: unique user identifier
-     */
     @PluginMethod
     public void setUserId(PluginCall call) {
-        if (!call.hasOption("userId")) {
-            call.reject("userId is missing");
+        String userId = call.getString("userId");
+        if (userId == null) {
+            call.reject(ERROR_USERID_MISSING);
             return;
         }
-
-        String userId = call.getString("userId");
-        com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setUserId(userId);
+        implementation.setUserId(userId);
         call.resolve();
     }
 
-    /**
-     * Adds logging that is sent with your crash data. The logging does not appear in the system.log and is only visible in the Crashlytics dashboard.
-     * @param call - message: message - message to display during logging
-     */
     @PluginMethod
     public void addLogMessage(PluginCall call) {
-        if (!call.hasOption("message")) {
-            call.reject("message property is missing");
+        String message = call.getString("message");
+        if (message == null) {
+            call.reject(ERROR_MESSAGE_MISSING);
             return;
         }
-
-        String message = call.getString("message");
-
-        com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().log(message);
+        implementation.addLogMessage(message);
         call.resolve();
     }
 
-    /**
-     * Enables/disables automatic data collection.
-     * @param call - enabled: boolean true/false to enable/disable crashlytics logging
-     */
     @PluginMethod
     public void setEnabled(PluginCall call) {
-        Boolean enabled = call.getBoolean("enabled", false);
-
-        com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(enabled);
+        Boolean enabled = call.getBoolean("enabled");
+        if (enabled == null) {
+            call.reject(ERROR_ENABLED_MISSING);
+            return;
+        }
+        implementation.setEnabled(enabled);
         call.resolve();
     }
 
-    /**
-     * Indicates whether or not automatic data collection is enabled
-     * @param call
-     * @return enabled: boolean true/false
-     */
     @PluginMethod
     public void isEnabled(PluginCall call) {
         call.unimplemented("Not implemented on Android.");
     }
 
-    /**
-     * Returns whether the app crashed during the previous execution.
-     * @param call
-     * @return crashed: boolean true/false
-     */
     @PluginMethod
     public void didCrashDuringPreviousExecution(PluginCall call) {
-        call.resolve(
-            new JSObject().put("crashed", com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().didCrashOnPreviousExecution())
-        );
+        boolean crashed = implementation.didCrashDuringPreviousExecution();
+        JSObject ret = new JSObject();
+        ret.put("crashed", crashed);
+        call.resolve(ret);
     }
 
-    /**
-     * Enqueues any unsent reports on the device to upload to Crashlytics.
-     * This method only applies if automatic data collection is disabled.
-     * @param call
-     */
     @PluginMethod
     public void sendUnsentReports(PluginCall call) {
-        com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().sendUnsentReports();
+        implementation.sendUnsentReports();
         call.resolve();
     }
 
-    /**
-     * Deletes any unsent reports on the device.
-     * This method only applies if automatic data collection is disabled.
-     * @param call
-     */
     @PluginMethod
     public void deleteUnsentReports(PluginCall call) {
-        com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().deleteUnsentReports();
+        implementation.deleteUnsentReports();
         call.resolve();
     }
 
-    /**
-     * Records a non-fatal report to send to Crashlytics.
-     * If automatic data collection is disabled, this method queues up all the reports on a device to send to Crashlytics.
-     * @param call
-     */
     @PluginMethod
     public void recordException(PluginCall call) {
-        if (!call.hasOption("message")) {
-            call.reject("message property is missing");
+        String message = call.getString("message");
+        if (message == null) {
+            call.reject(ERROR_MESSAGE_MISSING);
             return;
         }
-
-        String message = call.getString("message");
-        com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance().recordException(new Throwable(message));
+        implementation.recordException(message);
         call.resolve();
     }
 }
