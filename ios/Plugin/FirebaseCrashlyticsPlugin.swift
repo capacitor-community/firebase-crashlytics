00d640a13272a0abdf604062740b13a0b31052d4
Robin Genz <mail@robingenz.dev>
2021-04-21 17:50:58 +0200

    refactor(ios): split plugin and implementation (#34)

--- ios/Plugin/FirebaseCrashlyticsPlugin.swift
+++ ios/Plugin/FirebaseCrashlyticsPlugin.swift
@@ -1,7 +1,5 @@
 import Foundation
 import Capacitor
-import FirebaseCore
-import FirebaseCrashlytics
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,161 +7,96 @@ import FirebaseCrashlytics
  */
 @objc(FirebaseCrashlyticsPlugin)
 public class FirebaseCrashlyticsPlugin: CAPPlugin {
+    public let errorMessageMissing = "message must be provided.";
+    public let errorKeyMissing = "key must be provided.";
+    public let errorValueMissing = "value must be provided.";
+    public let errorUserIdMissing = "userId must be provided.";
+    public let errorEnabledMissing = "enabled must be provided.";
+    private var implementation: FirebaseCrashlytics?
     
     public override func load() {
-        if (FirebaseApp.app() == nil) {
-            FirebaseApp.configure()
-        }
+        implementation = FirebaseCrashlytics()
     }
     
-    
-    /// Trigger a fatal crash for crashlytics to record
-    /// - Parameter call: none
     @objc func crash(_ call: CAPPluginCall) {
         call.resolve()
-        
-        fatalError()
+        implementation?.crash()
     }
     
-    
-    /// Sets a custom key and value to be associated with subsequent fatal and non-fatal reports. When setting an object value, the object is converted to a string.
-    /// - Parameter call: key - unique key
-    ///                   value - value for the key to record
     @objc func setContext(_ call: CAPPluginCall) {
-        if !call.hasOption("key") || !call.hasOption("value") {
-            call.reject(!call.hasOption("key") ? "key propery is missing" : "value propery is missing")
-            return
+        guard let key = call.getString("key") else {
+            call.reject(errorKeyMissing)
+            return;
         }
-        
-        let key = call.getString("key")
+        let hasValue = call.hasOption("value")
+        if hasValue == false {
+            call.reject(errorValueMissing)
+            return;
+        }
         let type = call.getString("type") ?? "string"
-        
-        switch type {
-        case "string":
-            Crashlytics.crashlytics().setCustomValue(call.getString("value") as Any, forKey: key!)
-            break
-            
-        case "int":
-            Crashlytics.crashlytics().setCustomValue(call.getInt("value") as Any, forKey: key!)
-            break
-            
-        case "boolean":
-            Crashlytics.crashlytics().setCustomValue(call.getBool("value") as Any, forKey: key!)
-            break
-            
-        case "long":
-            Crashlytics.crashlytics().setCustomValue(call.getInt("value") as Any, forKey: key!)
-            break
-            
-        case "double":
-            Crashlytics.crashlytics().setCustomValue(call.getDouble("value") as Any, forKey: key!)
-            break
-            
-        case "float":
-            Crashlytics.crashlytics().setCustomValue(call.getFloat("value") as Any, forKey: key!)
-            break
-            
-        default:
-            Crashlytics.crashlytics().setCustomValue(call.getString("value") as Any, forKey: key!)
-        }
+        implementation?.setContext(key, type, call)
         call.resolve()
     }
     
-    
-    /// Adds logging that is sent with your crash data. The logging does not appear in the system.log and is only visible in the Crashlytics dashboard.
-    /// - Parameter call: message - message to display during logging
     @objc func addLogMessage(_ call: CAPPluginCall) {
-        if !call.hasOption("message") {
-            call.reject("message property is missing")
-            return
+        guard let message = call.getString("message") else {
+            call.reject(errorMessageMissing)
+            return;
         }
-        
-        let message = call.getString("message")
-        Crashlytics.crashlytics().log(message!)
+        implementation?.addLogMessage(message)
         call.resolve()
     }
     
-    
-    
-    /// Records a user ID (identifier) that’s associated with subsequent fatal and non-fatal reports.
-    /// - Parameter call: userId - unique user identifier
     @objc func setUserId(_ call: CAPPluginCall) {
-        if !call.hasOption("userId") {
-            call.reject("userId property is missing")
-            return
+        guard let userId = call.getString("userId") else {
+            call.reject(errorUserIdMissing)
+            return;
         }
-        
-        let userId = call.getString("userId")
-        Crashlytics.crashlytics().setUserID(userId!)
+        implementation?.setUserID(userId)
         call.resolve()
     }
-    
-    
-    /// Enables/disables automatic data collection.
-    /// - Parameter call: enabled - boolean true/false to enable/disable crashlytics logging
+
     @objc func setEnabled(_ call: CAPPluginCall) {
-        let enabled = call.hasOption("enabled") ? call.getBool("enabled") : false;
-        Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(enabled!)
+        guard let enabled = call.getBool("enabled") else {
+            call.reject(errorEnabledMissing)
+            return;
+        }
+        implementation?.setEnabled(enabled)
         call.resolve()
     }
     
-    
-    /// Indicates whether or not automatic data collection is enabled
-    /// - Parameter call: none
-    /// - Parameter return: enabled - boolean true/false if crashlytics is enabled or disabled
     @objc func isEnabled(_ call: CAPPluginCall) {
+        let enabled = implementation?.isEnabled()
         call.resolve([
-            "enabled": Crashlytics.crashlytics().isCrashlyticsCollectionEnabled()
+            "enabled": enabled!
         ])
     }
     
-    /// Returns whether the app crashed during the previous execution.
-    /// - Parameter call: none
-    /// - Parameter return: crashed - boolean true/false
     @objc func didCrashDuringPreviousExecution(_ call: CAPPluginCall) {
+        let crashed = implementation?.didCrashDuringPreviousExecution()
         call.resolve([
-            "crashed": Crashlytics.crashlytics().didCrashDuringPreviousExecution()
+            "crashed": crashed!
         ])
     }
     
-    /// Enqueues any unsent reports on the device to upload to Crashlytics.
-    /// This method only applies if automatic data collection is disabled.
-    /// - Parameter call: none
     @objc func sendUnsentReports(_ call: CAPPluginCall) {
-        if Crashlytics.crashlytics().isCrashlyticsCollectionEnabled() {
-            call.reject("cannot send report while crashlytics is enabled")
-            return;
-        }
-        
-        Crashlytics.crashlytics().sendUnsentReports()
+        implementation?.sendUnsentReports()
         call.resolve()
     }
     
-    /// Deletes any unsent reports on the device.
-    /// This method only applies if automatic data collection is disabled.
-    /// - Parameter call: none
     @objc func deleteUnsentReports(_ call: CAPPluginCall) {
-        if Crashlytics.crashlytics().isCrashlyticsCollectionEnabled() {
-            call.reject("cannot send report while crashlytics is enabled")
-            return;
-        }
-        
-        Crashlytics.crashlytics().deleteUnsentReports()
+        implementation?.deleteUnsentReports()
         call.resolve()
     }
     
-    /// Records a non-fatal report to send to Crashlytics.
-    /// If automatic data collection is disabled, this method queues up all the reports on a device to send to Crashlytics.
-    /// - Parameter call: code - the error code
-    ///                   domain - a string containing the error domain
-    ///                   message - message to record for non-fatal crash
     @objc func recordException(_ call: CAPPluginCall) {
-        
+        guard let message = call.getString("message") else {
+            call.reject(errorMessageMissing)
+            return;
+        }
         let domain = call.getString("domain") ?? ""
         let code = call.getInt("code") ?? -1001
-        let message = call.getString("message") ?? ""
-        
-        Crashlytics.crashlytics().record(error: NSError(domain: domain, code: code, userInfo: [ NSLocalizedDescriptionKey: message]))
+        implementation?.recordException(message, domain, code)
         call.resolve()
     }
 }
